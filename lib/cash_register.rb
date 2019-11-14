47ab98e18f39d15ace3ac44219a87fa9409a8a1d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 02:17:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last
+  
+  def initialize(total = 0, discount = 0)
+    @total = total
+    @discount = discount
+    @items = []
+  end
 
+  def total
+    @total
+  end
+
+  #def discount
+  #  @discount
+  #end
+
+  def add_item(item, price, quantity = 1)
+    @total += (price * quantity)
+    @last = (price * quantity)
+    @items << item
+  end
+
+  def apply_discount
+    if discount > 0
+      @total -= (total * discount / 100)
+      puts "After the discount, the total comes to $#{total}."
+    else
+      'There is no discount to apply.'
+    end
+  end
+
+  def void_last_transaction
+    @total -= last
+  end
+end
