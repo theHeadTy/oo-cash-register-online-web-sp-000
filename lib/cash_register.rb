ae6baeff2ee296c654d0f7ec0e95078e4b9430c9
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-14 02:27:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -30,7 +30,7 @@ class CashRegister
       @total -= (total * discount / 100)
       return "After the discount, the total comes to $#{total}."
     end
-    return 'There is no discount to apply.'
+    'There is no discount to apply.'
     end
   end
 
